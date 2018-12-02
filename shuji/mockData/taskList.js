b0f6ae7818c24b1c7d0bb4a2273c0552e0f2fe7f
wuhk <wuhoukan@126.com>
2018-12-02 19:20:49 +0800

    [shuji] 把创建时间数据格式改为number

--- shuji/mockData/taskList.js
+++ shuji/mockData/taskList.js
@@ -1,17 +1,17 @@
 var response = {
-	date: [
+	data: [
 		{
-			createAt: "1543740243",
+			createAt: 1543740243,
 			id: "1543740243",
 			name: "我的模拟任务1",
 		},
 		{
-			createAt: "1543740253",
+			createAt: 1543740253,
 			id: "1543740253",
 			name: "我的模拟任务2",
 		},
 		{
-			createAt: "1543658400",
+			createAt: 1543658400,
 			id: "1543658400",
 			name: "我的模拟任务3",
 		},
