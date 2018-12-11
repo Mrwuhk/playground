db69956428cf381ece1919de4677e92c90508126
wuhk <wuhoukan@126.com>
2018-12-11 20:34:59 +0800

    [vue-shuji] 新建一个Vue项目，重构数计项目

---
+++ vue-shuji/index.js
@@ -0,0 +1,34 @@
+new Vue({
+  el: '#app',
+  data: function() {
+		return {
+			tasks: [],
+		};
+	},
+	created: function() {
+		this.tasks = response.data.map((task, index, tasks) => {
+			return {
+				...task,
+				isHover: false,
+			};
+		});
+	},
+	methods: {
+		getDate: function(task){
+			const createAt=task.createAt;
+			return new Date(createAt).getFullYear()+'/'
+				+(new Date(createAt).getMonth()+1 )+'/'+
+				new Date(createAt).getDate() ;
+		},
+		mouseover: function(index) {
+			this.tasks = [
+				...this.tasks.slice(0, index),
+				{
+					...this.tasks[index],
+					isHover: true,
+				},
+				...this.tasks.slice(index + 1),
+			];
+		},
+	},
+})
